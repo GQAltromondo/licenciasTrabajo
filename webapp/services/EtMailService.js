1f4d94d86581c0895fa5df5f3ff82be0743a6c4d
Guillermo Quattrocchi <ext-gquattro>
2025-01-02 16:26:26 +0000

    Fix47

--- webapp/services/EtMailService.js
+++ webapp/services/EtMailService.js
@@ -68,6 +68,65 @@ sap.ui.define([
 				})
 			})
 		},
+		getPromiseTramitacion: function (empresa, estacion, seleccion,licencia) {
+			let entity = this._entitySet;
+			let filters = [
+				new sap.ui.model.Filter({
+					path: "Empresa",
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: empresa
+				}),
+				new sap.ui.model.Filter({
+					path: "Et",
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: estacion
+				}),
+
+			];
+			// Issue 582 - Se debe enviar correo a Tecnicos  de extremos de lineas cuando la ET es de tipo L ( Linea )
+
+			console.log("LicenseModel", AppManagementHelper.getModel("LicenseJsonModel").getData())
+
+
+			var sEquipo = licencia.getProperty("/Equnr");
+		//FIX GQ
+			if (!sEquipo) return []
+
+			let oEquipo = AppManagementHelper.getModel("EquiposJsonModel").getProperty("/Equipos").find((oEquipo) => oEquipo
+				.CodigoEquipo === sEquipo);
+			if (oEquipo) {
+				if (oEquipo.ExtremoA) {
+					filters.push(new sap.ui.model.Filter({
+						path: "Et",
+						operator: sap.ui.model.FilterOperator.EQ,
+						value1: oEquipo.ExtremoA
+					}));
+				}
+
+				if (oEquipo.ExtremoB) {
+					filters.push(new sap.ui.model.Filter({
+						path: "Et",
+						operator: sap.ui.model.FilterOperator.EQ,
+						value1: oEquipo.ExtremoB
+					}));
+				}
+			}
+
+			if (seleccion) {
+				filters.push(new sap.ui.model.Filter({
+					path: "Area",
+					operator: sap.ui.model.FilterOperator.EQ,
+					value1: seleccion
+				}));
+			}
+			return new Promise((resolve, reject) => {
+				oDataService.getModel("TransenerOperaciones").read(entity, {
+					filters: filters,
+					success: resolve,
+					error: reject
+				})
+			})
+		},
 
 	};
 });
