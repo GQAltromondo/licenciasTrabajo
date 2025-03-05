e0f107b908653d2c1ec41011fdb46cf396dad258
Guillermo Quattrocchi <ext-gquattro>
2025-03-05 13:38:24 +0000

    Doc 9 unifilares

--- webapp/views/Main/License/License.controller.js
+++ webapp/views/Main/License/License.controller.js
@@ -4243,7 +4243,7 @@ sap.ui.define([
 			if (!aLimitaciones) return obj
 			// Filtrar el array
 			var aFilteredLim = aLimitaciones.filter(limitacion => {
-					return limitacion.Estacion === sEquipo;
+				return limitacion.Estacion === sEquipo;
 			});
 
 			if (aFilteredLim?.length == 0) return
