84cf22f9f858e84b6eb058f5a76407c4858a0658
Guillermo Quattrocchi <ext-gquattro>
2025-10-23 18:21:01 +0000

    Test

--- webapp/services/LicenseService.js
+++ webapp/services/LicenseService.js
@@ -2908,7 +2908,7 @@ sap.ui.define([
 			oDataService.getModel("TransenerOperaciones").read(entity, {
 				filters: aFilters,
 				urlParameters: oParameter ? oParameter : {
-					"$select": "Nombre,Idunifilar,NumVersion,Region,TipoUnifilar,Et,Empresa,Anio,Region,IntAbLe,SecAbBt,SecPatCr,PatAdic,Numerolicencia"
+					"$select": "Nombre,Idunifilar,NumVersion,Region,TipoUnifilar,Et,Empresa,Anio,Region,IntAbLe,SecAbBt,SecPatCr,PatAdic,Numerolicencia,RealIdUnifilar"
 				},
 				success: fnCallback,
 				error: fnError
--- webapp/views/Main/Main.controller.js
+++ webapp/views/Main/Main.controller.js
@@ -767,7 +767,7 @@ sap.ui.define([
 								}, () => {
 									MessageBoxHelper.showAlert("Alerta", "Error al obtener unifilares")
 								}, {
-									"$select": "Nombre,Idunifilar,NumVersion,Region,TipoUnifilar,Et,Empresa,Anio,Region,IntAbLe,SecAbBt,SecPatCr,PatAdic,Numerolicencia,Mapa,Doctype,Imagenunifilar"
+									"$select": "Nombre,Idunifilar,NumVersion,Region,TipoUnifilar,Et,Empresa,Anio,Region,IntAbLe,SecAbBt,SecPatCr,PatAdic,Numerolicencia,Mapa,Doctype,Imagenunifilar,RealIdUnifilar"
 								})
 							} else {
 								BusyDialogHelper.close();
@@ -919,6 +919,8 @@ sap.ui.define([
 					oUnifilarFromOldLicense.Et && e.TipoUnifilar === oUnifilarFromOldLicense.TipoUnifilar)
 				if (oUnifilarNewVersion) {
 					if (oUnifilarFromOldLicense.NumVersion !== oUnifilarNewVersion.NumVersion) {
+						//GQ PROBLEMA CON DUPLICAR UNIFILARES 22/10
+						oUnifilarFromOldLicense.RealIdUnifilar = oUnifilarNewVersion.IdUnifilar
 						aUnifilaresToCreate.push({
 							data: oUnifilarFromOldLicense,
 							create: false
